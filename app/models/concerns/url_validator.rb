5ff7a2861677ce06d7624c5238b0e9a5b88d6d2e
Goldenson <orefice.maxime@gmail.com>
2017-04-20 19:26:08 -0400

    more refactor

--- app/models/concerns/url_validator.rb
+++ app/models/concerns/url_validator.rb
@@ -9,6 +9,7 @@ module UrlValidator
   def ensure_valid_url
     validates_with Validator
     return unless valid?
+
     invalid_url = @article.parsed_response.nil? || @article.parsed_response['error']
     return errors[:url] << 'your url suck bitch!' if invalid_url
   end
@@ -16,13 +17,11 @@ module UrlValidator
   class Validator < ActiveModel::Validator
     def validate(record)
       return record.errors[:url] << 'your url suck bitch!' if record.url.nil?
+
       begin
         uri = URI.parse(record.url)
-        response = uri.is_a?(URI::HTTP) && !uri.host.nil?
+        uri.is_a?(URI::HTTP) && !uri.host.nil?
       rescue URI::InvalidURIError
-        response = false
-      end
-      unless response
         record.errors[:url] << 'your url suck bitch!'
       end
     end
