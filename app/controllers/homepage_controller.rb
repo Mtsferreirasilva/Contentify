4de1231f054197c9e864336c7510fc11d4451de7
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-04-21 16:35:12 -0400

    Missing frozen string literal comment

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class HomepageController < ApplicationController
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-  root :to => 'homepage#index'
+  root to: 'homepage#index'
 
   resources :reader, only: [:index]
   resources :library, only: [:index]
