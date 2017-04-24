e617c3f57c1edd4634e1e86426ffa3d3b4dc9056
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-04-24 00:03:44 -0400

    Removing extension

--- Contentify-Extension/popup.js
+++
@@ -1,4 +0,0 @@
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    window.open("https://contentify.herokuapp.com/reader?url=" + url);
-});
