0c775818f22d2752fdec611d943fede082c636b4
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-04-27 20:41:23 -0400

    Fixing prefered host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  HEROKU_HOST = 'contentify.herokuapp.com'
   PREFERED_HOST = 'www.contentify.io'
 
   before_filter :redirect_to_prefered_host
@@ -15,7 +16,7 @@ class ApplicationController < ActionController::Base
   private
 
   def redirect_to_prefered_host
-    if Rails.env.production? && request.host != PREFERED_HOST
+    if Rails.env.production? && request.host == HEROKU_HOST
       redirect_to(host: PREFERED_HOST)
     end
   end
