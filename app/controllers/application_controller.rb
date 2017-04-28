6fdfe9ef3b52bf60de24b4d0496c6bed6e376a4a
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-04-27 20:53:53 -0400

    Trying to fix the herokuapp host redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,10 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
-  HEROKU_HOST = 'contentify.herokuapp.com'
-  PREFERED_HOST = 'www.contentify.io'
-
-  before_filter :redirect_to_prefered_host
-
   protect_from_forgery with: :exception
 
   helper_method :current_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  private
-
-  def redirect_to_prefered_host
-    if Rails.env.production? && request.host == HEROKU_HOST
-      redirect_to(host: PREFERED_HOST)
-    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+    r301 'contentify.herokuapp.com', 'contentify.io'
+  end
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
