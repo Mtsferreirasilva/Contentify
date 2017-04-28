24c07557a0f7766d8d2c559a37e9e07c8aa69b53
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-04-27 23:59:34 -0400

    Theme crontrols

--- app/javascript/reader.js
+++ app/javascript/reader.js
@@ -2,10 +2,12 @@ import progressBar from './modules/progressBar';
 import readerNavbar from './modules/readerNavbar';
 import popover from './modules/popover';
 import fontSize from './modules/fontSize';
+import theme from './modules/theme';
 
 window.reader = function reader() {
   progressBar();
   readerNavbar();
   popover();
   fontSize();
+  theme();
 }
