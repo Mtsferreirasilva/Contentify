99e3c379838f0f92b7fcbf9263214460e91ad912
Goldenson <orefice.maxime@gmail.com>
2017-04-29 17:18:28 -0400

    beginning of saving articles

--- app/controllers/reader_controller.rb
+++ app/controllers/reader_controller.rb
@@ -1,13 +1,26 @@
 # frozen_string_literal: true
 class ReaderController < ApplicationController
+  before_action :authenticate_user!, only: [:save_article]
+
   def index
     @url = reader_params[:url]
     @article = ArticleParser.new(@url)
   end
 
+  def save_article
+    # Handle authentication
+    article = JSON.parse(reader_params[:article])
+    binding.pry
+    current_user.articles.create(content: article)
+    @url = reader_params[:url]
+    @article = ArticleParser.new(reader_params[:url])
+    # render js response instead
+    render :index
+  end
+
   private
 
   def reader_params
-    params.permit(:url)
+    params.permit(:url, :article)
   end
 end
