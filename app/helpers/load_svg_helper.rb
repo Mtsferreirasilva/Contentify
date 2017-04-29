2b29fac351cd7300b7ec8c10d630ac57c0909114
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-04-29 13:59:25 -0400

    comments

--- app/helpers/load_svg_helper.rb
+++ app/helpers/load_svg_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module LoadSvgHelper
   def svg(path)
-    File.open("app/assets/images/#{path}", "rb") do |file|
+    File.open("app/assets/images/#{path}", "r") do |file|
       raw file.read
     end
   end
