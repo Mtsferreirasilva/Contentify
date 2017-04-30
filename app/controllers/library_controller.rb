9fa92f102c243d4fd9eaaec16ea50238773ef5cb
Goldenson <orefice.maxime@gmail.com>
2017-04-30 18:31:59 -0400

    populate library index page for users

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 class LibraryController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @articles = Article.where(user_id: current_user.id)
   end
 end
