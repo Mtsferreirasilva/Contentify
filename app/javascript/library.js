a52001028c484c5b0299a57f8fc985f052dcf330
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-08-26 17:33:24 -0400

    adding local storage

--- app/javascript/library.js
+++ app/javascript/library.js
@@ -3,11 +3,13 @@ import fontSize from './modules/fontSize';
 import theme from './modules/theme';
 import fab from './modules/fab';
 
+const NAVBAR_CONTROLS_ITEM_CLASS = 'navbar__controls-item'
+
 window.Contentify.Library = class Library {
   constructor() {
     fontSize();
     theme();
-    popover('navbar__controls-item');
+    popover(NAVBAR_CONTROLS_ITEM_CLASS);
     fab();
   }
 }
