c31b4feb24764c14939ef790fef9e1b51d8d598f
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-08-27 01:39:17 -0400

    trying https

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class HomepageController < ApplicationController
   def index
-    @brand_link = request.protocol + request.url
+    @brand_link = request.url
     @extension_url = 'https://chrome.google.com/webstore/detail/contentify/amfoahkobkpngjhnofkneikkcodeeldb?hl=en-US'
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.routes.default_url_options = 'https'
 end
