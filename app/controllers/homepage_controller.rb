20b35d1c33563be3b8b51991e9dc774eac44b6c4
mtsferreirasilva <mtsferreirasilva@gmail.com>
2018-02-27 02:43:53 -0500

    adding about it link to gh

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,5 +3,6 @@ class HomepageController < ApplicationController
   def index
     @brand_link = 'https://www.contentify.io'
     @extension_url = 'https://chrome.google.com/webstore/detail/contentify/amfoahkobkpngjhnofkneikkcodeeldb?hl=en-US'
+    @project_link = 'https://github.com/mateus/Contentify'
   end
 end
